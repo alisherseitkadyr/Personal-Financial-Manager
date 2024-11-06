5d13c9f02c01db0533dbaa95d4caf1b0999abd08
Yerlanmm <130394795+Yerlanmm@users.noreply.github.com>
2024-11-06 15:24:31 +0500

    Update views.py

--- finance/views.py
+++ finance/views.py
@@ -1,47 +1,39 @@
-from django.shortcuts import render
+
 
 from django.shortcuts import render, redirect
-from .forms import ExpenseForm
-from .models import Expense, Income
-from .forms import IncomeForm
-from django.db.models import Sum
+from .forms import ExpenseForm, IncomeForm
+from .services import FinanceService
 
+finance_service = FinanceService()
 
 def expense_list(request):
-    expenses = Expense.objects.all()
-    return render(request, '../templates/expense_list.html', {'expenses': expenses})
+    expenses = finance_service.list_expenses()
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
 
 def add_expense(request):
     if request.method == 'POST':
         form = ExpenseForm(request.POST)
         if form.is_valid():
-            form.save()
+            finance_service.add_expense(form.cleaned_data)
             return redirect('expense_list')
     else:
         form = ExpenseForm()
-    return render(request, '../templates/add_expense.html', {'form': form})
+    return render(request, 'expenses/add_expense.html', {'form': form})
 
 def income_list(request):
-    incomes = Income.objects.all()
-    return render(request, '../templates/income_list.html', {'incomes': incomes})
+    incomes = finance_service.list_incomes()
+    return render(request, 'expenses/income_list.html', {'incomes': incomes})
 
 def add_income(request):
     if request.method == 'POST':
         form = IncomeForm(request.POST)
         if form.is_valid():
-            form.save()
+            finance_service.add_income(form.cleaned_data)
             return redirect('income_list')
     else:
         form = IncomeForm()
-    return render(request, '../templates/add_income.html', {'form': form})
+    return render(request, 'expenses/add_income.html', {'form': form})
 
 def home(request):
-    total_expenses = Expense.objects.aggregate(total=Sum('amount'))['total'] or 0
-    total_income = Income.objects.aggregate(total=Sum('amount'))['total'] or 0
-    balance = total_income - total_expenses
-
-    return render(request, '../templates/home.html', {
-        'total_expenses': total_expenses,
-        'total_income': total_income,
-        'balance': balance,
-    })
+    totals = finance_service.calculate_totals()
+    return render(request, 'expenses/home.html', totals)
