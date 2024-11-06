37d71c6ad481c45640a77b748716e6a16d9cb0aa
nikk1n <nik.moiseichenko.06@gmail.com>
2024-11-06 14:39:34 +0500

    Add bootstrap classes to templates

--- finance/forms.py
+++ finance/forms.py
@@ -1,13 +1,20 @@
 from django import forms
+from django.forms import ModelForm
+
 from .models import Expense
 from .models import Income
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['name', 'amount', 'date']
+        widgets = {'date': DateInput()}
 
 class IncomeForm(forms.ModelForm):
     class Meta:
         model = Income
         fields = ['name', 'amount', 'date']
+        widgets = {'date': DateInput()}
